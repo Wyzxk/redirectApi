28163110565ac6037a54ad1c96528d882575e2c5
Wyzxk <tkg.jhoncaro@gmail.com>
2024-08-08 18:00:27 -0500

    urls

--- redirect/settings.py
+++ redirect/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@4z^t7zmwggd(u*$36y8&fi3l@s=+b_re+lvbq%yhfwm$%qdo+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
